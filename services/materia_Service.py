59a4a3d5eccd0628bfe5efbe680f3e348f9800c3
Rafael Jose Porto Silva <rafaelportot@localhost.localdomain>
2026-03-28 17:01:58 -0300

    CRUD Materia

--- services/materia_Service.py
+++ services/materia_Service.py
@@ -1,4 +1,5 @@
 from models import db, Materia
+from datetime import datetime
 
 class Materia_Service():
     @staticmethod
@@ -11,3 +12,29 @@ class Materia_Service():
     @staticmethod
     def listar_tudo():
         return Materia.query.all()
+
+    @staticmethod
+    def buscar_por_id(materia_id):
+        return Materia.query.get(materia_id)
+        
+    
+    @staticmethod
+    def atualizar_materia(materia_id, novo_nome=None, nova_cor=None):
+        materia = Materia.query.get(materia_id)
+        if materia:
+            if nome:
+                materia.nome = novo_nome
+            if cor:
+                materia.cor = nova_cor
+            db.session.commit()
+        return None
+    
+    @staticmethod
+    def remover_materia(materia_id):
+        materia = Materia.query.get(materia_id)
+        if materia:
+            db.session.delete(materia)
+            db.session.commit()
+            return True
+
+        return False
