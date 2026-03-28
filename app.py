0bd7861395ff0f31ac22de1abde7b28358fc7130
Rafael Jose Porto Silva <rafaelportot@localhost.localdomain>
2026-03-28 17:37:28 -0300

    Blueprints no app

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from models import db 
 from routes.materia_Routes import materia_bp
+from routes.conteudo_Routes import conteudo_bp
+from routes.tarefa_Routes import tarefa_bp
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///estudos.db'
@@ -9,6 +11,8 @@ db.init_app(app)
 
 # Separação de rotas
 app.register_blueprint(materia_bp)
+app.register_blueprint(conteudo_bp)
+app.register_blueprint(tarefa_bp)
 
 if __name__ == '__main__':
     with app.app_context():
