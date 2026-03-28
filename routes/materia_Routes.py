e17fb0fb8790f4b5fa2a78e64e33ea63a39b7626
Rafael Jose Porto Silva <rafaelportot@localhost.localdomain>
2026-03-28 17:25:10 -0300

    crud materia - routes

--- routes/materia_Routes.py
+++ routes/materia_Routes.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, render_template
-from services.materia_Service import MateriaService
+from flask import Blueprint, render_template, request, redirect, url_for
+from services.materia_Service import Materia_Service
 
 materia_bp = Blueprint('materia', __name__)
 
@@ -8,8 +8,25 @@ def index():
     materias = Materia_Service.listar_tudo()
     return render_template('index.html', materias=materias)
 
+@materia_bp.route('/materia/<int:id>')
+def ver_materia(id):
+    materia = Materia_Service.buscar_por_id(id)
+    return render_template('materia.html', materia=materia)
+
 @materia_bp.route('/materia/nova', methods=['POST'])
 def nova():
     nome = request.form.get('nome')
-    Materia_Service.criar_materia(nome)
+    cor = request.form.get('cor', 'primary')
+    Materia_Service.criar_materia(nome, cor)
+    return redirect(url_for('materia.index'))
+
+@materia_bp.route('/materia/editar/<int:id>', methods=['POST'])
+def editar(id):
+    nome = request.form.get('nome')
+    Materia_Service.atualizar_materia(id, novo_nome=nome)
+    return redirect(url_for('materia.index'))
+
+@materia_bp.route('/materia/remover/<int:id>', methods=['POST'])
+def remover(id):
+    Materia_Service.remover_materia(id)
     return redirect(url_for('materia.index'))
